35fa5b88d7f7d205960f4e6a685f870e1ad072e3
Cameron Witcher <QuickScythe@gmail.com>
2015-12-01 18:45:56 -0600

    Fixed help command.

--- src/me/quickScythe/eridaunicore/commands/HelpCommand.java
+++ src/me/quickScythe/eridaunicore/commands/HelpCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import me.quickScythe.eridaunicore.Main;
+import me.quickScythe.eridaunicore.utils.Utils;
 
 public class HelpCommand implements CommandExecutor {
 	Main plugin;
@@ -16,7 +17,7 @@ public class HelpCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 		if (cmd.getName().equalsIgnoreCase("help")) {
-			// sender.sendMessage("");
+			sender.sendMessage(Utils.colorize("&e&lHelp &f>&7 If you need help please ask an Mod or higher, or submit a request with &f\"request\"&7."));
 		}
 		return true;
 	}
