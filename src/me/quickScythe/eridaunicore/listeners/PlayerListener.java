43be9362d6e913da34a7c5cd73c29658bb44c373
cynthia.crim <cynthia.crim@DESKTOP-8PI5DNJ>
2016-08-10 17:27:58 -0500

    noo

--- src/me/quickScythe/eridaunicore/listeners/PlayerListener.java
+++ src/me/quickScythe/eridaunicore/listeners/PlayerListener.java
@@ -1,12 +1,16 @@
 package me.quickScythe.eridaunicore.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.TravelAgent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.util.Vector;
 
 import me.quickScythe.eridaunicore.Main;
@@ -24,7 +28,12 @@ public class PlayerListener implements Listener {
 	}
 	
 	
-	
+	@EventHandler
+	public void onPlayerPortal(PlayerPortalEvent e){
+		if(e.getCause().equals(TeleportCause.END_PORTAL) && e.getPlayer().getWorld().equals(CoreUtils.getSpawn().getWorld())){
+			e.getPlayer().teleport(Bukkit.getWorld("world").getSpawnLocation());
+		}
+	}
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e){
 		PermissionUser player = PermissionsEx.getUser(e.getPlayer());
